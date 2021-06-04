6a24e65297efe7f2fb2e508bc633498812bf5cce
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-04 17:12:43 +0700

    refactor: add an action to gameFragment and complete clickHandler to the play button

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,7 +32,9 @@ class TitleFragment : Fragment() {
         )
 
         // Add a click handler to the Play button
-        binding.playButton.setOnClickListener {  }
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
