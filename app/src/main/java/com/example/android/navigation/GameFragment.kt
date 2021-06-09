8383d14596fdf3c3d90e3562b9dea311599f012f
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-09 21:49:27 +0700

    feat: display a toast to show correct number of answers

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -117,6 +118,10 @@ class GameFragment : Fragment() {
                 }
             }
         }
+
+        val args = GameWonFragmentArgs.fromBundle((requireArguments()))
+        Toast.makeText(context, "Correct: ${args.numCorrect}, out of ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 
